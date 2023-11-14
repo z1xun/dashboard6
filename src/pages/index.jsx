b936200214ce327f000ce19ef9843a0ccf0ad17d
eehd80 <eehd80yul@gmail.com>
2023-11-14 17:15:43 +0900

    line chart

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { MoonIcon } from '@chakra-ui/icons';
-import { SimpleGrid, Flex } from '@chakra-ui/react';
+import { SimpleGrid, Flex, Card, CardHeader, CardBody, Heading, Text } from '@chakra-ui/react';
 import Layout from '../components/layout/Layout';
 import MiniStatistics from '../components/home/MiniStatistics';
 import IconBox from '../components/icon/IconBox';
+import LineChart from '../components/charts/LineChart';
+import { lineChartDataOverallRevenue, lineChartOptionsOverallRevenue } from '../components/charts/CartData';
 
 const boxBg = 'secondary.500';
 
@@ -37,6 +39,26 @@ const Home = () => {
                 />
                 <MiniStatistics name="New Tasks" value="154" />
                 <MiniStatistics name="Total Projects" value="2935" />
+            </SimpleGrid>
+
+            <SimpleGrid columns={[1, 2]} spacing={'20px'} mt={'20px'}>
+                <Card>
+                    <CardHeader>
+                        <Heading size="md">Client Report</Heading>
+                    </CardHeader>
+                    <CardBody>
+                        <LineChart
+                            chartData={lineChartDataOverallRevenue}
+                            chartOptions={lineChartOptionsOverallRevenue}
+                        />
+                        <Text>View a summary of all your customers over the last month.</Text>
+                    </CardBody>
+                </Card>
+                <Card>
+                    <CardBody>
+                        <Text>View a summary of all your customers over the last month.</Text>
+                    </CardBody>
+                </Card>
             </SimpleGrid>
         </Layout>
     );
